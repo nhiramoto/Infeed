6b1c7803bcc1fd7cbc5f5bd65073132bf35ad094
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-04-29 17:13:17 -0300

    Created handler to add feed.

--- feed/urls.py
+++ feed/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 app_name = 'feed'
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', views.index, name='index'),
+    path('sources/add/', views.sources_add, name="add")
 ]
--- feed/views.py
+++ feed/views.py
@@ -1,5 +1,24 @@
 from django.shortcuts import render
 
+from .models import Source
+
 
 def index(request):
     return render(request, 'feed/index.html')
+
+
+def sources_add(request):
+    try:
+        name = request.POST['name']
+        description = request.POST['description']
+        url = request.POST['url']
+        source = Source(name=name, description=description, url=url)
+        source.save()
+    except (KeyError):
+        return render(request, 'feed/index.html', {
+            'error_message': 'Title or URL not provided.'
+        })
+    else:
+        return render(request, 'feed/index.html', {
+            'success_message': 'Source successfully added.'
+        })
