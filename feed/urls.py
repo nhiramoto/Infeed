98bd2234b51eb8262a10ea3f416c428e97637b5a
nhtoshiaki <nicolau.hiramoto@gmail.com>
2019-05-02 17:00:14 -0300

    Handling list addition.

--- feed/urls.py
+++ feed/urls.py
@@ -5,5 +5,6 @@ from . import views
 app_name = 'feed'
 urlpatterns = [
     path('', views.index, name='index'),
+    path('lists/add/', views.lists_add, name="lists_add"),
     path('sources/add/', views.sources_add, name="sources_add")
 ]
--- feed/views.py
+++ feed/views.py
@@ -13,6 +13,26 @@ def index(request, success_message=None, error_message=None):
     })
 
 
+def lists_add(request):
+    source_lists = SourceList.objects.all()
+    try:
+        name = request.POST['name']
+        if len(name) == 0:
+            raise KeyError
+        source_list = SourceList(name=name)
+        source_list.save()
+    except KeyError:
+        return render(request, 'feed/index.html', {
+            'source_lists': source_lists,
+            'error_message': 'Name not provided.'
+        })
+    else:
+        return render(request, 'feed/index.html', {
+            'source_lists': source_lists,
+            'success_message': f'List "{name}" successfully added.'
+        })
+
+
 def sources_add(request):
     source_lists = SourceList.objects.all()
     try:
@@ -20,6 +40,8 @@ def sources_add(request):
         description = request.POST['description']
         source_list_id = request.POST['source_list_id']
         url = request.POST['url']
+        if len(name) == 0 or len(url) == 0:
+            raise KeyError
         source_list = get_object_or_404(SourceList, pk=source_list_id)
         source_list.source_set.create(name=name, description=description,
                                       url=url)
@@ -27,7 +49,7 @@ def sources_add(request):
     except KeyError:
         return render(request, 'feed/index.html', {
             'source_lists': source_lists,
-            'error_message': 'Title, description or URL not provided.'
+            'error_message': 'Title or URL not provided.'
         })
     else:
         return render(request, 'feed/index.html', {
